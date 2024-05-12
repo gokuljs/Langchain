1089cf0352f12d60564828b12f05707c253cf328
gokuljs <gokuljs029@gmail.com>
2024-05-12 14:27:16 +0530

    convert data into embedding

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config();
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { Document } from "langchain/document";
+import { createClient } from "@supabase/supabase-js";
+import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
+import { OpenAIEmbeddings } from "@langchain/openai";
 
 function purifyText(content) {
   // Remove URLs
   content = content.replace(/https?:\/\/\S+/g, "");
-
   // Standardize and format certain sections
   content = content.replace("Athens User:", "User Type: Athens User");
-
   // Remove phone numbers
   content = content.replace(/\(\d{3}\) \d{3}-\d{4}/g, "[Phone Number Removed]");
-
   // Remove emails
   content = content.replace(/\S+@\S+\.\S+/g, "[Email Removed]");
-
   // Optional: Trim extra spaces and format newlines for readability
   content = content.replace(/\s+/g, " ").trim();
 
@@ -25,25 +25,40 @@ async function embeddedBloodReport() {
   try {
     const loader = new PDFLoader("./test.pdf");
     const docs = await loader.load();
+    const supabaseUrl = process.env.SUPABASE_URL;
+    const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+    const openAiKey = process.env.OPENAI_API_KEY;
     const splitter = new RecursiveCharacterTextSplitter({
       chunkSize: 1000,
       separators: ["\n\n", "\n", " ", "", "##", "[\n"],
       chunkOverlap: 190,
     });
-    const arr = [];
-    Array.isArray(docs) &&
-      docs.length > 0 &&
-      docs.forEach(async (item) => {
-        const docOutput = await splitter
-          .createDocuments([purifyText(item.pageContent)])
-          .then((item) => {
-            console.log(item);
-          });
-      });
-    console.log(arr);
+    let arr = [];
+    for (const item of docs) {
+      const docOutput = await splitter.createDocuments([
+        purifyText(item.pageContent),
+      ]);
+      arr = [...arr, ...docOutput];
+    }
+    const texts = arr.map((item) => item.pageContent);
+    const client = await createClient(supabaseUrl, supabaseAnonKey);
+    console.log(arr.length);
+    const text = arr.map((item) => item.pageContent);
+    const metaData = arr.map((item) => item.metadata);
+    const vectorStore = await SupabaseVectorStore.fromTexts(
+      text,
+      metaData,
+      new OpenAIEmbeddings({
+        openAIApiKey: openAiKey,
+      }),
+      {
+        client,
+        tableName: "documents",
+      }
+    );
   } catch (error) {
     console.log(error);
   }
 }
 
-embeddedBloodReport();
+// embeddedBloodReport();
